c5a54479cf83940630d6fa9c8a35410a6d16944d
Fynardo <dfynar@gmail.com>
2020-12-02 13:19:12 +0100

    day 1 part 2, kinda bruteforce

--- day1/expenses.py
+++ day1/expenses.py
@@ -8,13 +8,26 @@ def read_input():
 
 # Setup
 lines = read_input()
+wanted = 2020
 
 
 # Part 1
 for i, x in enumerate(lines):
     for j in range(i, len(lines)):
         y = lines[j]
-        if x + y == 2020:
+        if x + y == wanted:
             print(x*y)
 
+
+# Part 2
+cache = {}
+for i, x in enumerate(lines):
+    for j in range(i, len(lines)):
+        y = lines[j]
+        diff = wanted - x - y
+        try:
+            last = lines[lines.index(diff)]
+            print(x*y*last)
+        except ValueError:
+            continue
 
