08c0b9afb31c04e97a4a7eedd50cbea6fa11a5e8
Fynardo <dfynar@gmail.com>
2020-12-12 16:32:54 +0100

    day 12 part 2

--- day12/ferry.py
+++ day12/ferry.py
@@ -50,13 +50,76 @@ class Ship:
         self._long += _long_despl*value    
 
 
-
-# Part 1
 def manhattan(lat, long):
     return abs(lat) + abs(long)
 
+# Part 1
 print('Part 1')
 ferry = Ship()
+for (action, value) in navigation:
+    ferry.maneuver(action, value)
+
+print(f'\tSolution Found: {manhattan(*ferry.location)}')
+
+
+# Part 2
+class WaypointShip(Ship):
+    def __init__(self):
+        super().__init__()
+        self._waypoint_lat = 1
+        self._waypoint_long = 10
+        self._quadrants = [(1,1),(1,-1),(-1,-1),(-1,1)]
+        self._cur_quadrant = 0
+
+    def _rotate(self, cuad_offset):
+        if cuad_offset % 2 == 1:
+            self._waypoint_lat, self._waypoint_long = self._waypoint_long, self._waypoint_lat
+        self._waypoint_lat = abs(self._waypoint_lat) * self._quadrants[self._cur_quadrant][0]
+        self._waypoint_long = abs(self._waypoint_long) * self._quadrants[self._cur_quadrant][1]
+
+    def rotate_left(self, degrees):
+        cuad_offset = degrees//90
+        self._cur_quadrant = (self._cur_quadrant + cuad_offset) % 4
+        self._rotate(cuad_offset)
+
+    def rotate_right(self, degrees):
+        cuad_offset = degrees//90
+        self._cur_quadrant = (self._cur_quadrant - cuad_offset) % 4
+        self._rotate(cuad_offset)
+
+    def relocate(self):
+        if self._waypoint_lat > 0 and self._waypoint_long > 0:
+            self._cur_quadrant = 0
+        if self._waypoint_lat > 0 and self._waypoint_long < 0:
+            self._cur_quadrant = 1
+        if self._waypoint_lat < 0 and self._waypoint_long < 0:
+            self._cur_quadrant = 2
+        if self._waypoint_lat < 0 and self._waypoint_long > 0:
+            self._cur_quadrant = 3
+
+    def go_north(self, distance):
+        self._waypoint_lat += distance        
+        self.relocate()
+
+    def go_south(self, distance):
+        self._waypoint_lat -= distance
+        self.relocate()
+
+    def go_east(self, distance):
+        self._waypoint_long += distance
+        self.relocate()
+    
+    def go_west(self, distance):
+        self._waypoint_long -= distance
+        self.relocate()
+
+    def forward(self, value):
+        self._lat += value * self._waypoint_lat
+        self._long += value * self._waypoint_long
+
+
+print('Part 2')
+ferry = WaypointShip()
 for (action, value) in navigation:
     ferry.maneuver(action, value)
 
